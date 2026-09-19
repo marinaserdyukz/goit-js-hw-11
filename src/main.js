5521fa9fafe5acabe1dd40eaf3c7ceb9ae566573
maryna_serdiuk <serdyukmarina876@gmail.com>
2026-09-19 11:12:55 +0200

    updated main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { getImagesByQuery } from './pixabay-api';
+import { getImagesByQuery } from './js/pixabay-api';
+
 import {
   createGallery,
   clearGallery,
   showLoader,
   hideLoader,
-} from './render-functions';
+} from './js/render-functions';
 
 const form = document.querySelector('.form');
 
